9a64c6c49dfafd2fac40e89c3868ff21039bad95
Lucas Ferreira <lucasandradef44@gmail.com>
2026-04-29 20:49:37 -0300

    Adicionado Teste de conta poupança.

--- banco/src/test/java/banco/suite/BancoAllTestSuite.java
+++ banco/src/test/java/banco/suite/BancoAllTestSuite.java
@@ -3,10 +3,14 @@ package banco.suite;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
+import banco.dominio.ClienteTest;
+import banco.dominio.ContaCorrenteTest;
+import banco.dominio.ContaPoupancaTest;
+
+
 @Suite
 @SelectClasses({
     ClienteTest.class,
-    ContaTest.class,
     ContaCorrenteTest.class,
     ContaPoupancaTest.class
 })
